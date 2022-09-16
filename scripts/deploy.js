d4d6b6480b63bdd7ae9e323c53f621f930f800b6
Bozhidar Slaveykov <bobi@microweber.com>
2022-09-16 20:50:41 +0300

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,15 @@
-async function main () {
-  // We get the contract to deploy
-  const DriftManiaClub = await ethers.getContractFactory('DriftManiaClub');
-  console.log('Deploying DriftManiaClub...');
-  const dmcDeploy = await DriftManiaClub.deploy();
-  await dmcDeploy.deployed();
-  console.log('DriftManiaClub deployed to:', dmcDeploy.address);
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const DriftManiaClub = await ethers.getContractFactory("DriftManiaClub");
+    const token = await upgrades.deployProxy(DriftManiaClub, []);
+    await token.deployed();
+    console.log("Token deployed to:", token.address);
 }
 
 main()
     .then(() => process.exit(0))
     .catch(error => {
-      console.error(error);
-      process.exit(1);
+        console.error(error);
+        process.exit(1);
     });
